cd48784d9b372bc84b7af56e02be1efc629e7b70
mbarmawi <muhammadalbarmawi@outlook.com>
2017-03-08 08:25:03 +0300

    Format durm.js document

--- 01 - Drum Kit/drum.js
+++ 01 - Drum Kit/drum.js
@@ -1,21 +1,19 @@
- 
  document.addEventListener("keydown", function (e) {
-            const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
-            if (key) {
-                const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-                audio.currentTime = 0;  // Replay the audio from the beginning
-                audio.play();
-                key.classList.add("playing");
-            }
-        })
+     if (key) {
+         const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+         audio.currentTime = 0; // Replay the audio from the beginning
+         audio.play();
+         key.classList.add("playing");
+     }
+ })
 
-        const keys = document.querySelectorAll(".key");
+ const keys = document.querySelectorAll(".key");
 
-        // Remove playing class when transition end
-        keys.forEach(k => k.addEventListener("transitionend", function (e) {
-            if (e.propertyName === "transform") {
-                k.classList.remove("playing"); 
-            }
-        })
-        );
+ // Remove playing class when transition end
+ keys.forEach(k => k.addEventListener("transitionend", function (e) {
+     if (e.propertyName === "transform") {
+         k.classList.remove("playing");
+     }
+ }));
